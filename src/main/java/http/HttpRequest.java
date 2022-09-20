5ae588a31058f80da54768ff601d6168d2a3e32a
jjonyo <jonghyuk96@cau.ac.kr>
2022-09-21 00:44:05 +0900

    HttpRequest 리팩토링
    HttpRequestParser를 추가함에 따라 HttpRequest생성 로직을 변경했습니다.

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -1,113 +1,56 @@
 package http;
 
-import exception.HttpRequestException;
-
-import java.io.BufferedReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-
-import utils.HttpRequestUtils;
 
 public class HttpRequest {
 
-  private final String method;
-  private final String url;
-  private final String requestUrl;
-  private final String version;
+  private final HttpRequestLine requestLine;
   private final HttpHeaders headers;
-  private final Map<String, String> params;
+  private final HttpParams params;
   //private final HttpBody body;
 
-  private static final String QUERY_SPLIT_STRING = "\\?";
 
-
-  private HttpRequest(String method, String url, String version, HttpHeaders headers, Map<String, String> params) {
-    this.method = method;
-    this.url = url;
-    this.requestUrl = url.split(QUERY_SPLIT_STRING)[0];
-    this.version = version;
+  private HttpRequest(HttpRequestLine requestLine, HttpHeaders headers, HttpParams params) {
+    this.requestLine = requestLine;
     this.headers = headers;
     this.params = params;
   }
 
   public static HttpRequest of(InputStream in) {
-    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-    String method, url, version;
+    HttpRequestParser httpRequestParser = new HttpRequestParser(in);
 
-    try {
-      String[] requestLine = br.readLine().split(" ");
-      method = requestLine[0];
-      url = requestLine[1];
-      version = requestLine[2];
-    } catch (Exception e) {
-      throw new HttpRequestException("HttpRequest 생성에 실패했습니다.");
-    }
+    HttpRequestLine requestLine = httpRequestParser.parseRequestLine();
+    HttpHeaders headers = httpRequestParser.parseRequestHeaders();
+    HttpParams params = httpRequestParser.parseRequestParams(requestLine);
 
-    HttpHeaders headers = parseHeaders(br);
-    Map<String, String> params = parseParams(url);
-
-    return new HttpRequest(method, url, version, headers, params);
+    return new HttpRequest(requestLine, headers, params);
   }
 
-  private static Map<String, String> parseParams(String url) {
-    String[] splitUrl = url.split(QUERY_SPLIT_STRING);
-
-    if (splitUrl.length > 1) {
-      return HttpRequestUtils.parseQueryString(splitUrl[1]);
-    }
-
-    return new HashMap<>();
-  }
-
-  public void addHeader(String key, String value) {
-    headers.addHeader(key, value);
-  }
-
-  private static HttpHeaders parseHeaders(BufferedReader br) {
-    HttpHeaders headers = new HttpHeaders();
-
-    try {
-      String line = br.readLine();
-      while (!"".equals(line)) {
-        String[] splitLine = line.split(": ");
-        headers.addHeader(splitLine[0], splitLine[1]);
-
-        line = br.readLine();
-        if (line == null) {
-          break;
-        }
-      }
-    } catch (Exception e) {
-      throw new HttpRequestException("HttpRequest 생성에 실패했습니다.");
-    }
-
-    return headers;
-  }
-
-  public String getMethod() {
-    return method;
-  }
-
-  public String getUrl() {
-    return url;
-  }
-
-  public String getRequestUrl() {
-    return requestUrl;
-  }
-
-  public String getVersion() {
-    return version;
+  public HttpRequestLine getRequestLine() {
+    return requestLine;
   }
 
   public HttpHeaders getHeaders() {
     return headers;
   }
 
-  public Map<String, String> getParams() {
+  public HttpParams getParams() {
     return params;
+  }
+
+  public String getRequestUrl() {
+    return requestLine.getRequestUrl();
+  }
+
+  public String getMethod() {
+    return requestLine.getMethod();
+  }
+
+  public String getUrl() {
+    return requestLine.getUrl();
+  }
+
+  public String getVersion() {
+    return requestLine.getVersion();
   }
 }
