016aa012a7d218019e4c4d406907af5e004565a3
jjonyo <jonghyuk96@cau.ac.kr>
2022-09-29 16:39:03 +0900

    HttpHeaders fix indent

--- src/main/java/http/HttpHeaders.java
+++ src/main/java/http/HttpHeaders.java
@@ -6,11 +6,11 @@ import java.util.List;
 import java.util.Map;
 
 public class HttpHeaders {
-    public static final String CONTENT_LENGTH = "Content-Length";
-    public static final String CONTENT_TYPE = "Content-Type";
-    public static final String COOKIE = "Cookie";
-    public static final String SET_COOKIE = "Set-Cookie";
-    public static final String LOCATION = "Location";
+  public static final String CONTENT_LENGTH = "Content-Length";
+  public static final String CONTENT_TYPE = "Content-Type";
+  public static final String COOKIE = "Cookie";
+  public static final String SET_COOKIE = "Set-Cookie";
+  public static final String LOCATION = "Location";
 
   private final Map<String, List<String>> headers = new HashMap<>();
 
